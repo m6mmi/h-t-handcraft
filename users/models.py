5ec4b7c99f274502f216e08f21658cdf105f2e18
TriinuN <triinu.niklus@gmail.com>
2024-09-18 09:52:56 +0300

    Added User model, PLEASE CHECK it !! :D ,

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
