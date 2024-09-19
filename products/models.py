ae752f958d51834d6ac818b72a0f5a9a968d948f
Indrek Kuusk <indrek.kuusk@gmail.com>
2024-09-19 09:58:42 +0300

    Product-model models.py 1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
+# Lisatud 19.09.24
+
 from django.db import models
 
-# Create your models here.
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(help_text='Product description')
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    image_path = models.CharField(max_length=255, blank=True, null=True)
+    stock = models.IntegerField(default=0, null=False)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
