fc2c02228dddaefea40d10dc0b1ef73aa8d4c9ed
TriinuN <triinu.niklus@gmail.com>
2024-09-19 10:18:06 +0300

    Changed views file a bit. Added Login form and changed registration form a bit

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
@@ -11,3 +12,20 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30, required=True)
+    password = forms.CharField(max_length=30, required=True, widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username does not exist.")
+        return username
--- users/views.py
+++ users/views.py
@@ -1,25 +1,23 @@
 from urllib import request
-
 from django.shortcuts import render, redirect
-
-# Create your views here.
 from django.views import View
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from .forms import RegistrationForm
+
+# Create your views here.
 
 
 class RegisterView(View):
     def get(self, request):
         form = RegistrationForm()
         print(form)
-        return render(request, 'templates/register.html', {'form': form})
+        return render(request, 'register.html', {'form': form})
 
     def post(self, request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
+            user = form.save(commit=False)
             user.first_name = form.cleaned_data.get('first_name')
             user.last_name = form.cleaned_data.get('last_name')
             user.email = form.cleaned_data.get('email')
@@ -28,32 +26,29 @@ class RegisterView(View):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
-
-            return redirect('accounts:login')
-        return render(request, 'templates/register.html', {'form': form})
+            if user:
+                login(request, user)
+                return redirect('accounts:login')
+            return render(request, 'register.html', {'form': form})
 
 
 class LoginView(View):
     def get(self, request):
         form = AuthenticationForm()
-        return render(request, 'templates/login.html', {'form': form})
+        return render(request, 'login.html', {'form': form})
 
     def post(self, request):
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-
             return redirect('templates:profile')
         else:
-
             print(form.errors)
-        return render(request, 'templates/login.html', {'form': form})
+        return render(request, 'login.html', {'form': form})
 
 
 class LogoutView(View):
     def get(self, request):
         logout(request)
-
         return redirect('users:login')
