23359c622b8ea9cf39f8ba93a7d57521dfec8d82
Janek <m6mmikiisu@gmail.com>
2024-09-21 18:36:18 +0300

    Adding models to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Subcategory, Product
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Product)
--- users/models.py
+++ users/models.py
@@ -1,16 +1,5 @@
-
+from django.contrib.auth.models import User
 from django.db import models
-
-
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.username
 
 
 class Address(models.Model):
