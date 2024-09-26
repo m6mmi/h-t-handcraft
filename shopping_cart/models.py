e613fbbee858a2983982a173cf54184d9497ff51
Janek <m6mmikiisu@gmail.com>
2024-09-26 16:06:50 +0300

    Simple orders view

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -24,7 +24,7 @@ class Order(models.Model):
     confirmation = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Order id: {self.order_id}, user: {self.user_id}, date ordered: {self.date_ordered.strftime("%Y-%m-%d %H:%M:%S")}'
+        return f'Order id: {self.order_id}, date ordered: {self.date_ordered.strftime("%Y-%m-%d %H:%M:%S")}'
 
 
 class CartProduct(models.Model):
