4b01871e2ee1a6fffdd9867bef1dc6af80ccea8e
Janek <m6mmikiisu@gmail.com>
2024-10-29 22:38:27 +0200

    Add dynamic shipping price calculation.
    Integrated total price calculation based on selected shipping method. Updated both template and JavaScript to reflect changes, including adding new shipping options and container elements.

--- shopping_cart/templates/static/js/shipping_choice.js
+++ shopping_cart/templates/static/js/shipping_choice.js
@@ -6,17 +6,31 @@ const itellaContainer = document.getElementById('itella-container');
 const omnivaContainer = document.getElementById('omniva-container');
 const dpdContainer = document.getElementById('dpd-container');
 
+const totalCart = document.getElementById('total-cart-price');
+const shippingPriceContainer = document.getElementById('shipping-price-container');
+const totalPriceContainer = document.getElementById('total-price-container');
+
+let itellaPrice = 5;
+let omnivaPrice = 10;
+let dpdPrice = 15;
+
+// default values
 itellaRadio.checked = false;
 omnivaRadio.checked = true;
 itellaContainer.style.display = 'none';
 omnivaContainer.style.display = 'block';
 dpdContainer.style.display = 'none';
+shippingPriceContainer.innerText = omnivaPrice;
+calcTotal(totalCart, shippingPriceContainer);
+
 
 itellaRadio.addEventListener('change', () => {
   if (itellaRadio.checked) {
     itellaContainer.style.display = 'block';
     omnivaContainer.style.display = 'none';
     dpdContainer.style.display = 'none';
+    shippingPriceContainer.innerText = itellaPrice;
+    calcTotal(totalCart, shippingPriceContainer);
   }
 });
 
@@ -25,6 +39,8 @@ omnivaRadio.addEventListener('change', () => {
     itellaContainer.style.display = 'none';
     omnivaContainer.style.display = 'block';
     dpdContainer.style.display = 'none';
+    shippingPriceContainer.innerText = omnivaPrice;
+    calcTotal(totalCart, shippingPriceContainer);
   }
 });
 
@@ -33,5 +49,12 @@ dpdRadio.addEventListener('change', () => {
     itellaContainer.style.display = 'none';
     omnivaContainer.style.display = 'none';
     dpdContainer.style.display = 'block';
+    shippingPriceContainer.innerText = dpdPrice;
+    calcTotal(totalCart, shippingPriceContainer);
   }
 });
+
+function calcTotal(cart, shipping) {
+  total = parseFloat(cart.innerText ) + parseFloat(shipping.innerText);
+  return totalPriceContainer.innerText = total;
+}
--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from shopping_cart.views import DeleteFromCart, CartView, Checkout, ShippingAddressView, ShippingItella, ShippingIOmniva
+from shopping_cart.views import DeleteFromCart, CartView, Checkout, ShippingAddressView, ShippingItella, \
+    ShippingIOmniva, ShippingDPD
 
 app_name = "shopping_cart"
 
@@ -10,6 +11,7 @@ urlpatterns = [
     path("cart/", CartView.as_view(), name="cart"),
     path("checkout/", Checkout.as_view(), name="checkout"),
     path("shipping/", ShippingAddressView.as_view(), name="shipping"),
+    path("shipping_dpd/", ShippingDPD.as_view(), name="dpd"),
     path("shipping-itella/", ShippingItella.as_view(), name="itella"),
     path("shipping-omniva/", ShippingIOmniva.as_view(), name="omniva"),
 ]
