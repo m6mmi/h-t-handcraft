4db0867c8776c7bab9939635e0cb3e562dab6350
Janek <m6mmikiisu@gmail.com>
2024-10-29 20:22:07 +0200

    Changed DPD to DPD Kuller
    - Remover Address option
    - Deleted dpd.html and address option from shipping. Updated JavaScript and HTML to reflect the change. Renamed aadress.html to dpd_kuller.html.

--- shopping_cart/utils.py
+++ shopping_cart/utils.py
@@ -4,7 +4,6 @@ from users.views import InvoiceView
 
 def send_invoice_email(user):
     order = Order.objects.get(user_id=user, cart__is_active=True)
-    print(order)
     invoice_view = InvoiceView()
     pdf_file_path = invoice_view.generate_pdf(order)
     invoice_view.send_order_confirmation(order, pdf_file_path)
