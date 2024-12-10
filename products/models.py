7c385e278daaa5b338f6324497d6ef0eb5ab8fe7
TriinuN <triinu.niklus@gmail.com>
2024-12-10 11:55:00 +0200

    - commented out description

--- products/models.py
+++ products/models.py
@@ -10,19 +10,20 @@ class Category(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
-    description = models.TextField(help_text='Product description', null=True, blank=True)
+    #description = models.TextField(help_text='Product description', null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     image_path = models.ImageField(upload_to='products/img', null=True, blank=True)
     stock = models.IntegerField(default=0, null=False, blank=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    # height = models.FloatField(null=True, blank=True)
-    # width = models.FloatField(null=True, blank=True)
-    # length = models.FloatField(null=True, blank=True)
-    # weight = models.FloatField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    width = models.FloatField(null=True, blank=True)
+    length = models.FloatField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         category_name = Category.objects.get(id=self.category.id)
         return f'{category_name} --- {self.title}, Laos: {self.stock}'
+
 
 
 class GalleryImage(models.Model):
